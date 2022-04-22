1318ce590b0e9b07e6a1106c026eb33fe315e79d
Ryan Davis <ryand-ruby@zenspider.com>
2022-04-22 15:46:50 -0800

    prepped for release [git-p4: depot-paths = "//src/omnifocus/dev/": change = 13365]

--- lib/omnifocus.rb
+++ lib/omnifocus.rb
@@ -30,7 +30,7 @@ include Appscript
 # bts_id: a string uniquely identifying a task: SYSTEM(-projectname)?#id
 
 class OmniFocus
-  VERSION = "2.7.0"
+  VERSION = "2.7.1"
 
   class << self
     attr_accessor :description, :current_desc
