efc94eb82725bbed6560eb1a6b3232282fcb2108
Ryan Davis <ryand-ruby@zenspider.com>
2023-02-13 10:48:09 -0800

    - Removed patch for #to_ary bug from ruby 1.9 days. [git-p4: depot-paths = "//src/omnifocus/dev/": change = 13687]

--- lib/omnifocus.rb
+++ lib/omnifocus.rb
@@ -5,19 +5,6 @@ require "yaml"
 
 NERD_FOLDER = ENV["OF_FOLDER"] || "nerd"
 
-class Appscript::Reference # :nodoc:
-  # HACK: There is apparently a bug in ruby 1.9 where if you have
-  # method_missing defined and do some action that calls to_ary, then
-  # to_ary will be called on your instance REGARDLESS of whether
-  # respond_to_(:to_ary) returns true or not.
-  #
-  # http://tenderlovemaking.com/2011/06/28/til-its-ok-to-return-nil-from-to_ary/
-
-  def to_ary # :nodoc:
-    nil
-  end
-end if RUBY_VERSION >= "1.9"
-
 include Appscript
 
 ##
